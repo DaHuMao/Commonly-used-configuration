7fcd534a7f9433661b4d63d758e3dd16d901cdfe
zhangtongxiaobj <zhangtongxiaobj@yuanfudao.com>
2022-11-30 10:33:48 +0800

    MOD: git fzf时候对于新增文件预览使用bat

--- dotfiles/zsh/git-edit.zsh
+++ dotfiles/zsh/git-edit.zsh
@@ -1,7 +1,11 @@
 #! /bin/zsh
 local str=(${=2})
 if (( $1 == 2 )) {
-  eval "git diff ${str[$1]} | diff-so-fancy --colors"
+  if [[ $str[1] == '??' ]] {
+    eval "${FZF_FILE_HIGHLIGHTER} ${str[2]}"
+  } else {
+    eval "git diff ${str[2]} | diff-so-fancy --colors"
+  }
 } elif (( $1 == 1 )){
-  eval "git show ${str[$1]} | diff-so-fancy --colors"
+  eval "git show ${str[1]} | diff-so-fancy --colors"
 }
