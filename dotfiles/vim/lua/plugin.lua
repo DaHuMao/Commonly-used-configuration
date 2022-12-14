5c868eac2d4cdd2b070bc304bbbf3bec8767146d
zhangtongxiaobj <zhangtongxiaobj@yuanfudao.com>
2022-12-14 23:29:56 +0800

    [ADD]: plug buffer-cmp

--- dotfiles/vim/lua/plugin.lua
+++ dotfiles/vim/lua/plugin.lua
@@ -58,7 +58,7 @@ return packer.startup(function(use)
   use { "williamboman/mason.nvim" }
   --cmp
   use "hrsh7th/nvim-cmp" -- The completion plugin
-  --use "hrsh7th/cmp-buffer" -- buffer completions
+  use "hrsh7th/cmp-buffer" -- buffer completions
   use "hrsh7th/cmp-path" -- path completions
   use "hrsh7th/cmp-cmdline" -- cmdline completions
   use "saadparwaiz1/cmp_luasnip" -- snippet completions
