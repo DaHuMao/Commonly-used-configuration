0b4b3f1b82143c17407a33be46152eb1a7283dba
zhangtongxiaobj <zhangtongxiaobj@kanyun.com>
2023-10-14 13:34:47 +0800

    ADD: lsp log超过1M就自动删除

--- dotfiles/vim/lua/plug_config/lsp/init.lua
+++ dotfiles/vim/lua/plug_config/lsp/init.lua
@@ -1,3 +1,15 @@
+local log_filename = os.getenv("HOME") .. "/.local/state/nvim/lsp.log"
+local log_file = io.open(log_filename, "r")
+if log_file then
+    local size = log_file:seek("end")
+    log_file:close()
+    if size > 1048576 then
+      log_file = io.open(log_filename, "w")
+      if log_file then
+        log_file:close()
+      end
+    end
+end
 local status_ok, _ = pcall(require, "lspconfig")
 if not status_ok then
   print("error: can not find lspconfig")
